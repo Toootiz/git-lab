4b213fe9a5d0f6a8bbb6c93cbca94537a958de73
Gabriel <Toootiz.killer@gmail.com>
2024-04-15 14:11:33 -0600

    Added power py

---
+++ power_program.py
@@ -0,0 +1,13 @@
+import math
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+result = math.pow(a, b)
+print(f'{a} to the power of {b} is {result}')import math
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+result = math.pow(a, b)
+print(f'{a} to the power of {b} is {result}')
